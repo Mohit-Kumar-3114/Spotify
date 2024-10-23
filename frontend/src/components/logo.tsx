002586e43100c4bb3bf99fb2b9880e1947ae7e41
viratdogra1982 <viratd918@gmail.com>
2024-10-23 13:33:15 +0530

    hi

---
+++ frontend/src/components/logo.tsx
@@ -0,0 +1,9 @@
+const Logo=()=>{
+    return (
+    <div>
+     <img src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" alt="Spotify Logo" className="w-32"></img>
+    </div>
+    )
+}
+
+export default Logo;
